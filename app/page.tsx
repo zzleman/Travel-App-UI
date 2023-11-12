b550b9b77c732fe3bf5681073fc3754ad35ff49a
Leman <leman@Leman-MacBook-Air.local>
2023-11-12 13:00:24 +0400

    Hero section is done

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import Camp from '@/components/Camp'
+import Features from '@/components/Features'
+import GetApp from '@/components/GetApp'
+import Guide from '@/components/Guide'
+import Hero from '@/components/Hero'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className='text-bold text-teal-500 text-2xl'>
-    </h1>
+    <>
+    <Hero/>
+    <Camp />
+    <Guide />
+    <Features />
+    <GetApp />
+    </>
   )
 }
